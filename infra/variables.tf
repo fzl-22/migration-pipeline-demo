6133cf24ca7d3d5a15b5ddf019d3e167892f3686
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2026-01-02 21:16:31 +0700

    fix: add access key and secret key

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,6 +4,18 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "aws_access_key" {
+  description = "The AWS access key."
+  type        = string
+  default     = null
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key."
+  type        = string
+  default     = null
+}
+
 variable "managed_by" {
   description = "The deployment manager."
   type        = string
